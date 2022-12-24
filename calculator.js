39f9699c1306ee46c848302ea7f8714126fb9c61
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-24 13:18:01 -0800

    TOP Step 2 & 3. Operator function and html calculator buttons

--- calculator.js
+++ calculator.js
@@ -1,1 +1,19 @@
 const body = document.body;
+const calculatorContainer = document.getElementById("calculatorContainer");
+let equal = 0;
+function plusOp(num1, num2) {
+  equal = num1 + num2;
+  return equal;
+}
+function minusOp(num1, num2) {
+  equal = num1 - num2;
+  return equal;
+}
+function mulOp(num1, num2) {
+  equal = num1 * num2;
+  return equal;
+}
+function divOp(num1, num2) {
+  equal = num1 / num2;
+  return equal;
+}
