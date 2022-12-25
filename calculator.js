69b5bd39766dad2cfece364f22be971a5fa260fb
George Atsushi Ehara <118401130+george-atsushi-ehara@users.noreply.github.com>
2022-12-24 16:06:30 -0800

    number buttons appears in sequence

--- calculator.js
+++ calculator.js
@@ -1,5 +1,16 @@
 const body = document.body;
 const calculatorContainer = document.getElementById("calculatorContainer");
+const userInput = document.getElementById("input");
+
+const numberButton = document.querySelectorAll(".numberBtn");
+
+numberButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    userInput.textContent += button.textContent;
+  })
+})
+
+
 let equal = 0;
 function plusOp(num1, num2) {
   equal = num1 + num2;
